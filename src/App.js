afdeb26161b988abaf466dd3d61e9551817a43f2
Krunal Patel <Pkrunal616@gmail.com>
2019-05-08 21:34:53 -0400

    getting started

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navigation from './componentas/Navigation/Navigation';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentas/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <nav>
+            <p>Sing Out</p>
+        </nav>
+    )
+}
+
+export default Navigation;
