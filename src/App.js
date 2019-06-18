492fb4a2b6327496b251253a5706efdab796fbc3
Krunal Patel <Pkrunal616@gmail.com>
2019-06-18 00:33:26 -0400

    addding Clarifai

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
 import './App.css';
+import Clarifai from 'clarifai';
 import Navigation from './componentas/Navigation/Navigation';
 import Logo from './componentas/Logo/Logo';
 import ImageURL from './componentas/ImageURL/ImageURL';
 import Rank from './componentas/Rank/Rank';
-// import Navigation from './componentas/ImageWithFace/ImageWithFace';
+import ImageWithFace from './componentas/ImageWithFace/ImageWithFace';
 import Particles from 'react-particles-js';
 import 'tachyons';
+
+const app = new Clarifai.App({
+  apiKey: '17469b807a7c413fab46cfa7c759b586'
+ });
 
 const particlesOption = {
   particles: {
@@ -24,6 +29,49 @@ const particlesOption = {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      ImageWithFace: '',
+      box: {}
+    }
+  }
+
+  oninputChange = (event) => {
+    this.setState({input: event.target.value});
+  } 
+
+  faceLocation = (data) => {
+     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+     const image = document.getElementById('inputImage');
+     const width = Number(image.width);
+     const hight = Number(image.hight);
+
+     return{
+       leftCol: clarifaiFace.left_Col * width,
+       topRow: clarifaiFace.top_Row * hight,
+       rightCol: width - (clarifaiFace.right_Col * width),
+       bottomCol: hight - (clarifaiFace.bottom_row * hight),
+     }
+  }
+
+  displayFaceBox = (box) =>{
+    this.setState({box: box});
+  }
+
+  onSubmit = () =>{
+    this.setState({ImageWithFace: this.state.input});
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function (response) {
+        this.displayFaceBox(this.faceLocation(response));
+      },
+      function(err){
+        console.log(err);
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
@@ -33,8 +81,8 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank/>
-        <ImageURL />
-        {/* <ImageWithFace /> */}
+        <ImageURL oninputChange={this.oninputChange} onSubmit={this.onSubmit} />
+        <ImageWithFace ImageWithFace={this.state.ImageWithFace}/>
       </div>
     );
   }
--- src/componentas/ImageWithFace/ImageWithFace.js
+++ src/componentas/ImageWithFace/ImageWithFace.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
-const ImageWithFace = () => {
+const ImageWithFace = ( {ImageWithFace} ) => {
     return(
-        <input />
+        <div className="center ma">
+            <div className="absplute mt2">
+                <img id='inputImage' alt="" src={ImageWithFace} width='500px' height='auto' />
+            </div>
+        </div>
     );
 }
 
